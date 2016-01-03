60c52438a84d489c143fcc36f912ced8eaa53c4b
Maneesh Mohan <mmaneesh619@gmail.com>
2016-01-03 16:00:26 +0530

    need to work on params with an array

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -23,12 +23,21 @@ end
 
 def index
 	@customer= Customer.all
+end
+def fetch_data
+	@customer= Customer.all
 	@data=@customer.fetch
 	if @data.nil?
 	   puts "did not get"
 	else
 	   puts "got it!!"
 	end
+	puts customer_params
+end
+
+def customer_params
+
+params.require(:customer).permit(:userId,:lid,:title,:body)
 end
 
 end
